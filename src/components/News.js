bad20add312a2ebc2369db1fe7fbfa670e349289
wang tong <wangtong@oneapm.com>
2017-03-15 18:34:41 +0800

    加了好多东西

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,66 @@
 import React,{Component} from 'react'
-import { Carousel } from 'antd'
+import { Carousel ,Row ,Col ,Icon ,Card} from 'antd'
+import '../assets/style/carousel.css'
+
+
+let data = []
+
 
 export default class MainPage extends Component{
+  state={
+    get : false
+  }
+  componentDidMount(){
+    fetch("http://localhost:3000/getNews5", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((data)=>{
+      return data.json()
+    }).then((result)=>{
+      result.map((res)=>{
+        data.push(res)
+      })
+      this.setState({get:true})
+    })
+  }
+
   render(){
     return(
-        <Carousel effect="fade" autoplay>
-          <div><h1>1</h1></div>
-          <div><h1>2</h1></div>
-          <div><h5>3</h5></div>
-          <div><h1>4</h1></div>
-        </Carousel>
+      <Row justify="space-around" align="middle">
+        <Col span={13}>
+          {
+            data.length==0?null:(
+              <Carousel effect="fade" autoplay>
+                {
+                  data.map((res)=>{
+                    if(res.img)
+                      return (<div><img src={"http://ofdukoorb.bkt.clouddn.com/"+res.img}/></div>)
+                  })
+                }
+              </Carousel>
+            )
+          }
+        </Col>
+        <Col span={9}>
+          {
+            data.length==0?null:(
+              <Card title="新闻" extra={<a href="#">More</a>}>
+                {data.map((res)=>{
+                  return (
+                    <div className="oneNews">
+                      <a href={"/#/?news="+res.id}>{res.title}</a>
+                      <p>{res.date}</p>
+                      <hr />
+                    </div>   
+                  )
+                })}
+              </Card>
+            )
+          }
+        </Col>
+      </Row>
     )
   }
 }
